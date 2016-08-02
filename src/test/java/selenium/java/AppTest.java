c812106e3e8d126d1e1e04785b90624c220cf9eb
Alejandro Sierra Duran <asierrdu@usersad.everis.int>
2016-08-02 18:22:32 -0300

    test 1 example

--- src/test/java/selenium/java/AppTest.java
+++ src/test/java/selenium/java/AppTest.java
@@ -2,6 +2,7 @@ package selenium.java;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -23,35 +24,18 @@ public class AppTest
     public void testApp(){
     	
     	System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		Map<String, String> mobileEmulation = new HashMap<String, String>();
-		mobileEmulation.put("deviceName", "Google Nexus 5");
-
-		Map<String, Object> chromeOptions = new HashMap<String, Object>();
-		chromeOptions.put("mobileEmulation", mobileEmulation);
-		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-		capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
-		WebDriver driver = new ChromeDriver(capabilities);
-
-        driver.get("http://www.google.com");
-        
-        WebElement element = driver.findElement(By.name("q"));
-
-        element.sendKeys("Guns And Roses");
-
-        element.submit();
-        
-        try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-
-        // Check the title of the page
-        System.out.println("Page title is: " + driver.getTitle());
-
-        driver.quit();
     	
-        assertTrue( true );
+		WebDriver driver = new ChromeDriver();
+	
+		driver.manage().window().maximize();
+		driver.get("http://appswls.entel.cl/mientel/login.action");
+		driver.findElement(By.id("msisdn")).clear();
+	    driver.findElement(By.id("msisdn")).sendKeys("981575687");
+	    driver.findElement(By.id("rut")).clear();
+	    driver.findElement(By.id("rut")).sendKeys("1.111.744-9");
+	    driver.findElement(By.id("pin")).clear();
+	    driver.findElement(By.id("pin")).sendKeys("9696");
+	    driver.findElement(By.id("entrar")).click();
+	    driver.close();
     }
 }
