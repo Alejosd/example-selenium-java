713973eb042deb6987739389d7c0237f450aa144
Alejandro Sierra Duran <asierrdu@usersad.everis.int>
2016-08-03 15:01:13 -0300

    error 2

--- src/test/java/selenium/java/AppTest.java
+++ src/test/java/selenium/java/AppTest.java
@@ -25,11 +25,11 @@ public class AppTest
 		driver.manage().window().maximize();
 		driver.get("http://appswls.entel.cl/mientel/login.action");
 		driver.findElement(By.id("msisdn")).clear();
-	    driver.findElement(By.id("msisdn")).sendKeys("96241997");
+	    driver.findElement(By.id("msisdn")).sendKeys("962419977");
 	    driver.findElement(By.id("rut")).clear();
 	    driver.findElement(By.id("rut")).sendKeys("249510076");
 	    driver.findElement(By.id("pin")).clear();
-	    driver.findElement(By.id("pin")).sendKeys("3294");
+	    driver.findElement(By.id("pin")).sendKeys("32");
 	    driver.findElement(By.id("entrar")).click();
 	    driver.close();
     }
