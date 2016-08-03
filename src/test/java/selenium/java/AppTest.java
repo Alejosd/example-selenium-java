9db09749336b2687609fc9a904ab4279c7763970
Alejandro Sierra Duran <asierrdu@usersad.everis.int>
2016-08-03 12:41:39 -0300

    add config pox.xml report html surefire-report

--- src/test/java/selenium/java/AppTest.java
+++ src/test/java/selenium/java/AppTest.java
@@ -1,15 +1,10 @@
 package selenium.java;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
+
 
 import junit.framework.TestCase;
 
